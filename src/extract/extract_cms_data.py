3e446b7e63a42984319aeed68275e258ecfb9235
adamsmartinfo <101344140+adamsmartinfo@users.noreply.github.com>
2026-07-09 19:30:28 +0000

    Upload extracted CMS hospital data to Cloud Storage

--- src/extract/extract_cms_data.py
+++ src/extract/extract_cms_data.py
@@ -2,10 +2,10 @@ import json
 
 from src.utils.cms_client import fetch_cms_data
 from src.utils.config import CMS_HOSPITAL_API_URL
-
+from src.load.upload_to_gcs import upload_file
 
 RAW_OUTPUT_PATH = "data/raw/hospital_general_information_sample.json"
-
+GCS_RAW_PATH = "raw/hospital_general_information_sample.json"
 
 def save_raw_json(records, output_path):
     with open(output_path, "w") as file:
@@ -19,7 +19,8 @@ def main():
     hospitals = fetch_cms_data(CMS_HOSPITAL_API_URL, limit=10)
 
     save_raw_json(hospitals, RAW_OUTPUT_PATH)
-
+    upload_file(RAW_OUTPUT_PATH, GCS_RAW_PATH)
+    
     print(f"Retrieved {len(hospitals)} hospital records.")
     print(f"Saved raw data to {RAW_OUTPUT_PATH}")
 
